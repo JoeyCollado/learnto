c6b3a95aa5e078b020bbd8bbf6920826313037ac
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-02-17 21:57:18 +0800

    added link to footer icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import { Link } from "lucide-react";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='h-40 w-screen bottom-0 bg-slate-600 flex flex-col'>
-      <ul className='flex gap-10 justify-end mr-10 mt-2'>
-        <li>Facebook</li>
-        <li>Facebook</li>
-        <li>Facebook</li>
+    <div className="h-40 w-screen bottom-0 bg-slate-600 flex flex-col">
+      <ul className="flex gap-10 justify-end mr-10 mt-2">
+        <Link>
+          <li>Facebook</li>
+        </Link>
+        <Link>
+          <li>Facebook</li>
+        </Link>
+        <Link>
+          <li>Facebook</li>
+        </Link>
       </ul>
-      <p className='text-center bottom-0 pt-24'>© All rights reserved LearnTo 2025 </p>
+      <p className="text-center bottom-0 pt-24">
+        © All rights reserved LearnTo 2025{" "}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
