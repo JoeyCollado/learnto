9df7fe7cb49cdaabe3a65ee4a4ef2f628cce7ad2
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-02-17 21:56:28 +0800

    footer phase 1

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='h-40 w-screen bottom-0 bg-slate-600'>
-      
+    <div className='h-40 w-screen bottom-0 bg-slate-600 flex flex-col'>
+      <ul className='flex gap-10 justify-end mr-10 mt-2'>
+        <li>Facebook</li>
+        <li>Facebook</li>
+        <li>Facebook</li>
+      </ul>
+      <p className='text-center bottom-0 pt-24'>© All rights reserved LearnTo 2025 </p>
     </div>
   )
 }
