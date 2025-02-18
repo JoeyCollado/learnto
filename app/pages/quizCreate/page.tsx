083b46a7084cf0786fc4171fef45c06b0634621a
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-02-18 22:19:44 +0800

    publish and preview buttons

--- app/pages/quizCreate/page.tsx
+++ app/pages/quizCreate/page.tsx
@@ -30,6 +30,12 @@ const page = () => {
    
    <div className='bg-slate-500 h-screen w-[90%] ml-[5%] mt-20 rounded-lg'>
     <h1 className='text-center pt-10 text-3xl'>Create Quiz Here</h1>
+
+    <div className='buttons flex-row flex gap-2 md:justify-end md:mr-10 justify-center mt-10'>
+       <button className='bg-slate-700 px-2 rounded-md py-1 flex '>Publish</button>
+       <button className='bg-slate-700 px-2 rounded-md py-1 flex'>Preview</button>
+    </div>
+
    </div>
     </>
   )
