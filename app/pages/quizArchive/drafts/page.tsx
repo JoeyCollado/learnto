fa7d79cc08babbf4ed77803db56f050d429ddd00
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-02-18 14:51:41 +0800

    drafts phase 1

--- app/pages/quizArchive/drafts/page.tsx
+++ app/pages/quizArchive/drafts/page.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import { Quiz } from "../published/types";
+import QuizCard from "@/app/components/QuizCard";
 
-const page = () => {
+const PublishedQuizzes = () => {
+  const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+
+  useEffect(() => {
+    fetch("/datas/published.json")
+      .then((res) => res.json())
+      .then((data: Quiz[]) => setQuizzes(data))
+      .catch((error) => console.error("Failed to fetch quizzes:", error));
+  }, []);
+
   return (
-    <div className='absolute'>
-      drafts
+    <div className="absolute md:ml-[25%] md:w-3/5 mt-3 w-screen h-[600px] bg-slate-600 rounded-md p-6">
+      {/* Scrollable container with hidden scrollbar */}
+      <div className="overflow-y-auto max-h-full custom-scroll">
+        {/* Responsive grid layout for quiz cards */}
+        <div className="text-center text-4xl py-2 pb-10">
+          Drafts
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {quizzes.map((quiz) => (
+            <QuizCard key={quiz.id} quiz={quiz} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default PublishedQuizzes;
