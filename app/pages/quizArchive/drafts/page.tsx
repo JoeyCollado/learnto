ac508060dabf044d17c375a4ef707bd679bb9518
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-02-19 18:28:40 +0800

    fix drafts

--- app/pages/quizArchive/drafts/page.tsx
+++ app/pages/quizArchive/drafts/page.tsx
@@ -19,7 +19,7 @@ const PublishedQuizzes = () => {
       <div className="overflow-y-auto max-h-full custom-scroll">
         {/* Responsive grid layout for quiz cards */}
         <div className="text-center text-3xl py-2 pb-10">
-          Published Quizzes
+          Drafts Quizzes
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {quizzes.map((quiz) => (
