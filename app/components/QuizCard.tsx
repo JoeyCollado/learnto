efe21867922b030a863d7c8842dfabd523b04738
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-02-26 14:35:53 +0800

    fix title size

--- app/components/QuizCard.tsx
+++ app/components/QuizCard.tsx
@@ -7,7 +7,7 @@ const QuizCard = ({ quiz, onDelete }: { quiz: Quiz; onDelete: (id: number) => vo
   return (
     <div className="bg-slate-400 shadow-md rounded-lg p-4 text-center flex flex-col justify-between h-full">
       <div>
-        <h2 className="text-lg font-bold mt-2">{quiz.title}</h2>
+        <h2 className="text-[30px] font-bold mt-2">{quiz.title}</h2>
         <p className="text-gray-600">Subject: {quiz.subject}</p>
         <p className="text-gray-600">Questions: {quiz.questions}</p>
         <p className="text-gray-600 text-sm">Date: {quiz.dateCreated}</p>
