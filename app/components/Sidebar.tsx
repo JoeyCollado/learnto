1d975848c04c948229e3a4ed980a26962f8b96cc
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-02-27 20:35:17 +0800

    remove hover on button

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -73,7 +73,7 @@ const Sidebar = () => {
         {/* Fixed Position Collapse Button */}
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="absolute bottom-5 left-4 flex items-center gap-2 hover:bg-blue-800 text-white px-4 py-2 rounded-lg w-max"
+          className="absolute bottom-5 left-4 flex items-center gap-2 text-white px-4 py-2 rounded-lg w-max"
         >
           <ChevronLeft size={20} className={`${!isOpen && "hidden"}`} />
           <ChevronRight size={20} className={`${isOpen && "hidden"}`} />
