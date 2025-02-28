7b5f11480d6f21dde579404160324ad32f009f0e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-02-28 12:50:51 +0800

    archived con color

--- app/pages/quizArchive/archived/page.tsx
+++ app/pages/quizArchive/archived/page.tsx
@@ -23,7 +23,7 @@ const ArchivedQuizzes = () => {
         <Sidebar />
 
         <div className="flex-1 p-6 mt-[5%]">
-          <div className="bg-slate-600 rounded-md p-6 max-h-[580px] overflow-y-auto custom-scroll">
+          <div className="bg-slate-800 rounded-md p-6 max-h-[580px] overflow-y-auto custom-scroll">
             <div className="text-center text-3xl py-2 pb-10">Archived Quizzes</div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
               {quizzes.map((quiz) => (
