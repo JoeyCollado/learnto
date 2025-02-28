d20f66a2770f769a570b460ed6344167c8c39b92
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-02-28 12:50:30 +0800

    draft con color

--- app/pages/quizArchive/drafts/page.tsx
+++ app/pages/quizArchive/drafts/page.tsx
@@ -43,7 +43,7 @@ const DraftQuizzes = () => {
         <Sidebar />
 
         <div className="flex-1 p-6 mt-[5%]">
-          <div className="bg-slate-600 rounded-md p-6 max-h-[580px] overflow-y-auto custom-scroll">
+          <div className="bg-slate-800 rounded-md p-6 max-h-[580px] overflow-y-auto custom-scroll">
             <div className="text-center text-3xl py-2 pb-10">Drafts</div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
               {drafts.map((draft) => (
