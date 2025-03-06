3a014b0c59bb2b54bb173eae7e8ace5bda81506d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-06 14:05:16 +0800

    cancel hover

--- app/pages/quizCreate/addQuestion/page.tsx
+++ app/pages/quizCreate/addQuestion/page.tsx
@@ -103,7 +103,7 @@ const AddQuestion = () => {
         ✅ Save Question
       </button>
 
-      <button className="bg-red-600 px-4 py-2 rounded-md mt-2 text-white" onClick={() => router.push("/pages/quizCreate")}>
+      <button className="bg-red-600 hover:bg-red-500 px-4 py-2 rounded-md mt-2 text-white" onClick={() => router.push("/pages/quizCreate")}>
         ❌ Cancel
       </button>
     </div>
