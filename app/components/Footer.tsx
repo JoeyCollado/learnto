1b98d9a6afdf4553b42136c8fa7647b3c67d31d9
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-06 08:35:52 +0800

    fixed darkmode logic

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,24 +1,31 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { Facebook, Github, Twitter } from "lucide-react";
+import { useTheme } from "./theme-context";
 
-const Footer = ({isdarkMode}) => {
+const Footer = () => {
+  const { isDarkMode } = useTheme(); // Get dark mode state from context
+
   return (
-    <div className={`h-[110px] w-screen bottom-0 ${isdarkMode ? "bg-slate-800" : "bg-slate-300"}  flex flex-col`}>
+    <div
+      className={`h-[110px] w-screen bottom-0 flex flex-col ${
+        isDarkMode ? "bg-slate-800 text-white" : "bg-slate-300 text-black"
+      }`}
+    >
       <ul className="flex gap-10 justify-end mr-14 mt-4">
         <Link href="https://github.com/JoeyCollado">
-          <Github className="hover:text-blue-700"/>
+          <Github className="hover:text-blue-700 transition-colors duration-300 cursor-pointer" />
         </Link>
         <Link href="#">
-          <Twitter className="hover:text-blue-700"/>
+          <Twitter className="hover:text-blue-700 transition-colors duration-300 cursor-pointer" />
         </Link>
         <Link href="https://www.facebook.com/joeyaibert31/">
-          <Facebook className="hover:text-blue-700"/>
+          <Facebook className="hover:text-blue-700 transition-colors duration-300 cursor-pointer" />
         </Link>
       </ul>
-      <p className="text-center pt-10">
-        © All rights reserved to LearnTo 2025
-      </p>
+      <p className="text-center pt-10">© All rights reserved to LearnTo 2025</p>
     </div>
   );
 };
