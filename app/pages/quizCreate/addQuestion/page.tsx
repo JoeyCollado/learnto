4c48662c5b15549d00960b3f475b17925df69f7b
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-03-06 14:03:19 +0800

    save question

--- app/pages/quizCreate/addQuestion/page.tsx
+++ app/pages/quizCreate/addQuestion/page.tsx
@@ -99,7 +99,7 @@ const AddQuestion = () => {
         + Add Option
       </button>
 
-      <button className="bg-blue-600 px-4 py-2 rounded-md mt-4" onClick={handleSubmit}>
+      <button className="bg-blue-600 px-4 py-2 rounded-md mt-4 text-white" onClick={handleSubmit}>
         ✅ Save Question
       </button>
 
